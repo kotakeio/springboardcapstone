db8aa8e9d662f02d278edb2ef5056df815720885
kotakeio <139043320+kotakeio@users.noreply.github.com>
2025-04-22 06:29:35 -0600

    Update timeBlocks.util.js

--- personal-agent-back/services/blocks/timeBlocks.util.js
+++ personal-agent-back/services/blocks/timeBlocks.util.js
@@ -19,9 +19,11 @@
 const dayjs    = require("dayjs");
 const utc      = require("dayjs/plugin/utc");
 const timezone = require("dayjs/plugin/timezone");
+const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(isSameOrBefore);
 
 // ─────────────── Core Functions ───────────────
 /**
